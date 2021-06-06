d93f3a8fbc1d158af13d71698e04726dac1cb970
Mark Henderson <henderson.mark@gmail.com>
2021-06-06 18:04:58 -0400

    metrics implementation for graph

--- src/metrics.js
+++ src/metrics.js
@@ -7,37 +7,38 @@ const register = new client.Registry()
 register.setDefaultLabels({ app: 'cerebro' })
 client.collectDefaultMetrics({ register })
 
-const uniqueEvents = new client.Counter({
+// TODO: Maybe we need to change these back to Counters, maybe not.
+const uniqueEvents = new client.Gauge({
   name: 'unique_events_processed',
   help: 'Number of unique events processed by Cerebro'
 })
 register.registerMetric(uniqueEvents)
 
-const pullRequests = new client.Counter({
+const pullRequests = new client.Gauge({
   name: 'pull_requests_processed',
   help: 'Count of total pull request events processed.'
 })
 register.registerMetric(pullRequests)
 
-const suitablePRs = new client.Counter({
+const suitablePRs = new client.Gauge({
   name: 'suitable_pull_requests_found',
   help: 'Number of suitable pull requests by Cerebro'
 })
 register.registerMetric(suitablePRs)
 
-const missIncludedLangs = new client.Counter({
+const missIncludedLangs = new client.Gauge({
   name: 'miss_included_langs',
   help: 'Pull request -> candidate miss based on target programming language'
 })
 register.registerMetric(missIncludedLangs)
 
-const missNonHireable = new client.Counter({
+const missNonHireable = new client.Gauge({
   name: 'miss_non_hireable',
   help: 'Pull request -> candidate miss candidate.hirable value'
 })
 register.registerMetric(missNonHireable)
 
-const candidatesFound = new client.Counter({
+const candidatesFound = new client.Gauge({
   name: 'candidates_found',
   help: 'Count of candidates found by Cerebro so far',
   labelNames: ['lang']
