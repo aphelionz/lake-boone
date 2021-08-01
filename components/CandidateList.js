b8dace3a23b5f16f0a55db857f127514c3e7ec1b
Mark Henderson <henderson.mark@gmail.com>
2021-08-01 13:35:25 -0400

    refactor(CandidateList): use candidate object properties

--- components/CandidateList.js
+++ components/CandidateList.js
@@ -1,36 +1,31 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+function truncatePRUrl (prHtmlUrl) {
+  const urlParts = prHtmlUrl.split('/')
+  return `${urlParts[3]}/${urlParts[4]}#${urlParts[6]}`
+}
+
 export default function CandidateList (props) {
-  const candidateItems = props.candidates.map(candidate => {
-    const {
-      avatar_url,
-      bio,
-      hireable,
-      blog,
-      twitter_username,
-      company,
-      location,
-      html_url,
-      login,
-      includedLangs,
-      prHtmlUrl
-    } = candidate
-
-    return (<>
-      <h3><Link href={ html_url }>{login}</Link></h3>
-      <Image src={ avatar_url } alt="" width="120" />
-      <div>{ bio }</div>
-      <div>{ includedLangs }</div>
-      { hireable ? <div>Hireable: ✔️</div> : null }
+  return props.candidates.map(candidate => (
+    <div key={candidate.login + candidate.prHtmlUrl}>
+      <h3><Link href={candidate.html_url}>{candidate.login}</Link></h3>
       <div>
-        <Link href={ blog }>web</Link>&nbsp;
-        <Link href={ '//twitter.com/' + twitter_username }>twitter</Link>&nbsp;
-        <Link href={ prHtmlUrl }>pull request</Link>
+        via <Link href={candidate.prHtmlUrl}>{truncatePRUrl(candidate.prHtmlUrl)}</Link>
       </div>
-      { company ? <div>Company: { company }, { location }</div> : null }
-    </>)
-  })
-
-  return candidateItems
+      <Image src={candidate.avatar_url} alt='' width='120' height='120' />
+      <div>{candidate.bio}</div>
+      <div>{candidate.includedLangs.join(', ')}</div>
+      {candidate.hireable ? <div>Hireable: ✔️</div> : null}
+      <div>
+        {candidate.blog ? <Link href={candidate.blog}>web</Link> : null}
+        {
+          candidate.twitter_username
+            ? <Link href={'//twitter.com/' + candidate.twitter_username}>twitter</Link>
+            : null
+        }
+      </div>
+      {candidate.company ? <div>Company: {candidate.company}, {candidate.location}</div> : null}
+    </div>
+  ))
 }
